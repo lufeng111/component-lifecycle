c0732523c7fef1ec22fb38b2cc0dea49347c01fd
lufeng111 <1836232210@qq.com>
2019-07-19 15:49:42 +0800

    DoCheck钩子的调用

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit, OnChanges {
+export class ChildComponent implements OnInit, OnChanges, DoCheck {
 
   @Input()
   greeting: string;
@@ -13,6 +13,15 @@ export class ChildComponent implements OnInit, OnChanges {
   @Input()
   // user 属相是一个对象，这个对象只有一个属性name, name是一个string的字符串
   user: {name: string};
+
+  // 用来存老的username
+  oldUsername: string;
+
+  // changeDetected 用来标记当前username 是不是发生了变化
+  changeDetected:boolean = false;
+
+  // noChangeCount 计数器
+  noChangeCount: number = 0
 
   message: string = "初始化消息";
 
@@ -27,6 +36,24 @@ export class ChildComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     console.log(JSON.stringify(changes, null, 2));
   }
+
+  ngDoCheck(): void {
+    if(this.user.name != this.oldUsername) {
+      this.changeDetected = true;
+      console.log("DoCheck:user.name从"+ this.oldUsername+ "变为"+ this.user.name);
+      this.oldUsername = this.user.name;
+    }
+
+    if(this.changeDetected) {
+      this.noChangeCount = 0;
+    } else {
+      this.noChangeCount = this.noChangeCount + 1;
+      console.log("DoCheck:user.name没变化时ngDoCheck方法已经被调用"+this.noChangeCount+"次");
+    }
+    this.changeDetected = false;
+
+  }
+
 
 
 }
